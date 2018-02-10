57668e2e6bdb668dfcf106252bb206cc88cb96d2
12pt <12pt@users.noreply.github.com>
2018-02-10 19:59:29 +0000

    Tidy up docs.

--- ivy-phpunit.el
+++ ivy-phpunit.el
@@ -24,19 +24,14 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
+;;
 ;; You should always be able to find the latest version here at <URL:https://github.com/12pt/ivy-phpunit.el/>
 ;; This project is inspired by helm-phpunit but also provides additional utility such as selecting classes to test,
 ;; and allowing for fast swapping between test classes with ivy-phpunit-list-test-classes.
-
-;; Usage:
-
-;; Require the package:
-;; (require 'ivy-phpunit)
-;; And then call either ivy-phpunit-test-function, ivy-phpunit-list-test-classes, or ivy-phpunit-test-class
+;;
+;; Require the package and then call either `ivy-phpunit-test-function', `ivy-phpunit-list-test-classes', or `ivy-phpunit-test-class'.
 
 ;;; Code:
-
 (require 'phpunit)
 (require 'ivy)
 
