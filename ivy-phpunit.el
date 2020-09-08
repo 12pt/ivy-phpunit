88e29b1d1c91aab209e26339df2ffcde0a9c7624
Naoya Yamashita <conao3@gmail.com>
2020-09-08 14:04:26 +0900

    depends Emacs 25.1 because use of directory-files-recursively

--- ivy-phpunit.el
+++ ivy-phpunit.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/12pt/ivy-phpunit
 ;; Version: 0.0.1
 ;; Keywords: convenience tools ivy phpunit php
-;; Package-Requires: ((ivy "0.10.0") (phpunit "0.7.0") (emacs "25"))
+;; Package-Requires: ((emacs "25.1") (ivy "0.10.0") (phpunit "0.7.0"))
 
 ;; This file is not part of GNU Emacs.
 
